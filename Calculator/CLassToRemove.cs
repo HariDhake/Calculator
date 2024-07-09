a851a2c34b611267e6cca2c78ffd62a4f71ca4de
HariDhake <dhakedharish6216@gmail.com>
2024-07-09 15:57:26 +0530

    Removal Class added

---
+++ Calculator/CLassToRemove.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class CLassToRemove
+    {
+        public void Remove() {
+            Console.WriteLine("removal code");
+        }
+    }
+}

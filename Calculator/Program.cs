03b76b06a9d41cb3f3fcd2a2e4a58aca0ce54855
HarshalShinde03 <harsh.shinde70@gmail.com>
2024-07-09 16:28:31 +0530

    class obj called

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -15,7 +15,9 @@ namespace Calculator
             Console.WriteLine("Addition is "+add);
             ViewerClass viewerClass = new ViewerClass();
             viewerClass.printTable(10);
+            timepass tp = new timepass();
             Console.ReadLine();
+
         }
     }
 }
